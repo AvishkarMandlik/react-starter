275c5627590d12d20963dcaa2fec81f97d86b297
AvishkarMandlik <mandlikavi121gmail.com>
2022-11-20 20:44:25 +0530

    to root div add and mango and apple function add

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import './mangondapple';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const root1 = ReactDOM.createRoot(document.getElementById('root1'));
+root.render(<Mango />);
+root1.render(<Apple />)
---
+++ src/mangondapple.js
@@ -0,0 +1,14 @@
+import './mangondapple';
+ 
+export function Mango() {
+  return (
+    <div className="Mango">Mango</div>
+  );
+}
+
+
+export function Apple(){
+  return(
+    <div>Apple</div>
+  );
+}
