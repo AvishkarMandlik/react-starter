d45a0292f3f74683ed837a5027332cb41673e602
AvishkarMandlik <mandlikavi121gmail.com>
2022-11-22 16:29:58 +0530

    index.js modified

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import './mangondapple';
+import App from "./App.js"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const root1 = ReactDOM.createRoot(document.getElementById('root1'));
-root.render(<Mango />);
-root1.render(<Apple />)
+root.render(<App />);
